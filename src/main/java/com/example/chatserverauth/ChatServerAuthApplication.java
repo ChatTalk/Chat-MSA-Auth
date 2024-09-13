9c1afcfb6f207810359f1fa7cfa43dcb2c9f459d
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-14 01:09:28 +0900

    initial setting

---
+++ src/main/java/com/example/chatserverauth/ChatServerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatserverauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServerAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServerAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatserverauth/ChatServerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatserverauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
