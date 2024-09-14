a9023a8ee539d2bb03bb2f3216b10c997ea2e2dd
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-14 20:19:26 +0900

    fix: 리액티브 카프카 설정 기반 수정

--- src/main/java/com/example/chatserverauth/global/config/ReactiveKafkaConsumerConfig.java
+++ src/main/java/com/example/chatserverauth/global/config/ReactiveKafkaConsumerConfig.java
@@ -3,20 +3,22 @@ package com.example.chatserverauth.global.config;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import reactor.kafka.receiver.ReceiverOptions;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 // API Gateway 에서 날 것의 엑세스 토큰(String)을 전달받기 위한 카프카 컨슈머 설정
 @EnableKafka
 @Configuration
-public class KafkaConsumerConfig {
+public class ReactiveKafkaConsumerConfig {
 
     @Value("${kafka.uri}")
     private String uri;
@@ -27,23 +29,25 @@ public class KafkaConsumerConfig {
     @Value("${kafka.auto-offset-reset}")
     private String autoOffsetReset;
 
+    @Value("${kafka.topic}")
+    private String topic;
+
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ReceiverOptions<String, String> kafkaReceiver(KafkaProperties kafkaProperties) {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, uri);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
 
-        return new DefaultKafkaConsumerFactory<>(props);
+        ReceiverOptions<String, String> basicReceiverOptions = ReceiverOptions.create(props);
+        return basicReceiverOptions.subscription(Collections.singletonList(topic));
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-
-        return factory;
+    public ReactiveKafkaConsumerTemplate<String, String> reactiveKafkaConsumer(ReceiverOptions<String, String> kafkaReceiverOptions) {
+        return new ReactiveKafkaConsumerTemplate<>(kafkaReceiverOptions);
     }
 }
