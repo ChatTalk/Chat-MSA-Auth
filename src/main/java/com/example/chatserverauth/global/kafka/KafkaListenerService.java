fdafb2adb71651bb7f4173e122c49511fed26367
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-22 01:36:01 +0900

    fix: 리액티브 기반 로직 철회 수정 완료

--- src/main/java/com/example/chatserverauth/global/kafka/KafkaListenerService.java
+++ src/main/java/com/example/chatserverauth/global/kafka/KafkaListenerService.java
@@ -2,10 +2,12 @@ package com.example.chatserverauth.global.kafka;
 
 import com.example.chatserverauth.domain.dto.TokenDTO;
 import com.example.chatserverauth.domain.dto.UserInfoDTO;
+import com.example.chatserverauth.global.auth.service.JwtParseService;
 import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -21,11 +23,17 @@ public class KafkaListenerService {
     @Value("${kafka.group-id}")
     private String groupId;
 
+    private final RedisTemplate<String, UserInfoDTO> userInfoTemplate;
     private final KafkaTemplate<String, UserInfoDTO> kafkaTemplate;
+    private final JwtParseService jwtParseService;
 
     @KafkaListener(topics = "result", groupId = "auth")
     public void listen(TokenDTO tokenDTO) {
         log.info("수신 토큰: {}", tokenDTO.getToken());
         if (tokenDTO.getToken() == null) throw new JwtException("엑세스 토큰이 존재하지 않습니다.");
+
+        UserInfoDTO userInfoDTO = jwtParseService.parseTokenWithCache(tokenDTO);
+        log.info("송신 파티션 키: {}\n송신 파싱 이메일: {}", tokenDTO.getId().toString(), userInfoDTO.getEmail());
+        kafkaTemplate.send("auth", tokenDTO.getId().toString(), userInfoDTO);
     }
 }
